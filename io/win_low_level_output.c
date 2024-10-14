1fa0e9723995f63bf0f62580e69956d536d6e792
unknown <yuraklisko@gmail.com>
2024-10-14 12:16:59 +0300

    io: edit win api output

--- io/win_low_level_output.c
+++ io/win_low_level_output.c
@@ -3,16 +3,16 @@
 int main()
 {
 	//~ Рядок, який необхідно вивести
-	char str[12] = "Hello World";
+	char str[14] = "Hello, World\n";
 
 	//~ Відкриваємо файл
 	int  * OutFile = CreateFileA("tmp.dat", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	//~ Записуємо у файл
-	WriteFile(OutFile, str, 12, NULL, NULL); 
+	WriteFile(OutFile, str, 14, NULL, NULL); 
 	
 	//~ Функція для отримання вказівника на номер стандартного потоку виведення
 	int * Out = GetStdHandle(STD_OUTPUT_HANDLE);
 	//~ Записуємо у стандартний потік виведення - у консоль
-	WriteFile(Out, str, 12, NULL, NULL);
+	WriteFile(Out, str, 14, NULL, NULL);
 	
 }
