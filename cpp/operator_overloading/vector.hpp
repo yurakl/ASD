af6cb69f18dbe12293073848d08cdf39b261b211
yurakl <yuraklisko@gmail.com>
2024-11-05 12:47:47 +0200

    oo: add const

--- cpp/operator_overloading/vector.hpp
+++ cpp/operator_overloading/vector.hpp
@@ -8,9 +8,9 @@ private:
     float x {1.0}, y {1.0}, z {1.0};
     
 public:
-    float get_x() {return x;};
-    float get_y() {return y;};
-    float get_z() {return z;};
+    float get_x() const {return x;};
+    float get_y() const {return y;};
+    float get_z() const {return z;};
     string name {"Unknown"}; 
 
     Vector() = default;
@@ -27,31 +27,31 @@ public:
 	friend int 		operator > (const Vector& v_1, const Vector& v_2);
 };
 
-Vector operator + (Vector& v_1, Vector& v_2)
+Vector operator + (const Vector& v_1, const Vector& v_2)
 {
 	Vector v_3 {v_1.x + v_2.x, v_1.y + v_2.y, v_1.z + v_2.z};
 	return v_3;
 }
 
-Vector operator + (Vector& v_1, float var)
+Vector operator + (const Vector& v_1, const float var)
 {
 	Vector v_3 {v_1.x + var, v_1.y + var, v_1.z + var};
 	return v_3;
 }
 
-Vector operator + (float var, Vector& v_1)
+Vector operator + (const float var, const Vector& v_1)
 {
 	Vector v_3 {v_1.x + var, v_1.y + var, v_1.z + var};
 	return v_3;
 }
 
-std::ostream& operator << (std::ostream& stream, Vector& v)
+std::ostream& operator << (std::ostream& stream, const Vector& v)
 {
 	stream << v.name << ": " << v.get_x() << ", " << v.get_y() << ", " << v.get_z() << endl;
 	return stream;
 }
 
-int operator > (Vector& v_1, Vector& v_2)
+int operator > (const Vector& v_1, const Vector& v_2)
 {
 	if ((v_1.x * v_1.x + v_1.y * v_1.y + v_1.z * v_1.z) > (v_2.x * v_2.x + v_2.y * v_2.y + v_2.z * v_2.z)) {
 		return 1;
